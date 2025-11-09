efd2c0a73bedb01177c690a75ea8a20b195bf1f8
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2025-11-09 02:06:51 -0600

    fix remove backend config cog

--- frontend/app/components/ChatInterface.tsx
+++ frontend/app/components/ChatInterface.tsx
@@ -6,7 +6,6 @@ import ChatMessages from './ChatMessages';
 import ChatInput from './ChatInput';
 import StripeCheckout from './StripeCheckout';
 import UserProfileIcon from './UserProfileIcon';
-import BackendUrlConfig from './BackendUrlConfig';
 import { useWebSocket } from '../hooks/useWebSocket';
 import { useNotifications } from '@/app/notifications/hooks';
 
@@ -176,9 +175,6 @@ export default function ChatInterface() {
       {isCheckoutOpen && (
         <StripeCheckout onClose={() => setIsCheckoutOpen(false)} />
       )}
-
-      {/* Backend URL Configuration */}
-      <BackendUrlConfig />
     </div>
   );
 }
