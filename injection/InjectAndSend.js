e6c18e0494d361640152b7b2d7b9287cb2366968
Ivan Yu <ivan@Ivans-MacBook-Pro.local>
2025-11-08 20:41:30 -0500

    refactor

--- injection/InjectAndSend.js
+++ injection/InjectAndSend.js
@@ -1,32 +1,33 @@
-const el = document.querySelector('.aislash-editor-input');
-el.focus();
+async function injectAndSend(newText) {
+  const el = document.querySelector('.aislash-editor-input');
+  el.focus();
 
-// Select all contents
-const sel = window.getSelection();
-const range = document.createRange();
-range.selectNodeContents(el);
-sel.removeAllRanges();
-sel.addRange(range);
+  // Select all contents
+  const sel = window.getSelection();
+  const range = document.createRange();
+  range.selectNodeContents(el);
+  sel.removeAllRanges();
+  sel.addRange(range);
 
-// Fire a genuine delete
-el.dispatchEvent(new InputEvent('beforeinput', {
-  inputType: 'deleteByCut',
-  data: null,
-  bubbles: true,
-  cancelable: true,
-}));
+  // Fire a genuine delete
+  el.dispatchEvent(new InputEvent('beforeinput', {
+    inputType: 'deleteByCut',
+    data: null,
+    bubbles: true,
+    cancelable: true,
+  }));
 
-// Now insert new text
-const newText = 'Tell me about the history of the 67 meme!';
-el.dispatchEvent(new InputEvent('beforeinput', {
-  inputType: 'insertText',
-  data: newText,
-  bubbles: true,
-  cancelable: true,
-}));
+  // Now insert new text
+  el.dispatchEvent(new InputEvent('beforeinput', {
+    inputType: 'insertText',
+    data: newText,
+    bubbles: true,
+    cancelable: true,
+  }));
 
-await new Promise(resolve => setTimeout(resolve, 500));
+  await new Promise(resolve => setTimeout(resolve, 500));
 
-const btn = document.querySelector('div.anysphere-icon-button[data-mode="agent"][data-outlined="true"]');
-console.log(btn);
-btn.click();
+  const btn = document.querySelector('div.anysphere-icon-button[data-mode="agent"][data-outlined="true"]');
+  console.log(btn);
+  btn.click();
+}
